cb6b734c04ce8e36b1f8ab31223160042b9b2799
Bryan Geedman <bgeedman@gmail.com>
2018-08-07 21:26:43 -0500

    Updated main file to use external interrupt and have the timers all configured. Still need to polish it up a bit so it is accurate though

--- clock.c
+++ clock.c
@@ -5,50 +5,161 @@
 #include <string.h>
 
 
-unsigned int extraTime = 0;
-unsigned char count = 0;
+#define RED_LED         PD3
+#define BLUE_LED        PD4
+#define GREEN_LED       PD5
+#define PWM_TICK_WIDTH  64
+#define PWM             PD7
 
+
+#define OFF             0x00
+#define RED             (1 << RED_LED)
+#define BLUE            (1 << BLUE_LED)
+#define GREEN           (1 << GREEN_LED)
+#define CYAN            (1 << BLUE_LED) | (1 << GREEN_LED)
+#define PURPLE          (1 << RED_LED) | (1 << BLUE_LED)
+#define YELLOW          (1 << RED_LED) | (1 << GREEN_LED)
+#define WHITE           (1 << RED_LED) | (1 << BLUE_LED) | (1 << GREEN_LED)
+
+
+#define RESOLUTION      180
+#define TICKS   5e-7
+
+
+
+static uint8_t colors[] = {
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+    GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+};
+
+
+
+void init_timers(void);
+void set_duty_cycle(uint16_t width);
+void init_ESC(void);
+void set_color(uint8_t color);
+
+
+uint8_t gCount = 0;
+uint8_t gPlatterPos = 0;
+uint8_t gRotations = 0;
+
+
+uint8_t cycle_color[] = {RED, PURPLE, BLUE, CYAN, GREEN, YELLOW, WHITE};
+
+/*
+ * Interrupt handler for dividing the clock face
+ */
 ISR(TIMER0_COMP_vect)
 {
-    extraTime++;
-    if (extraTime > 100)
+    gPlatterPos++;
+    if (gPlatterPos < RESOLUTION)
     {
-        extraTime = 0;
-        /* count++; */
-        PORTD ^= (1 << PD3);
+        set_color(colors[gPlatterPos]);
     }
+}
+
+
+/*
+ * Interrupt handler that should trigger every 100 ms
+ * After 10 iterations, recalculate the RPS and set OCR0 correctly
+ */
+ISR(TIMER1_COMPA_vect)
+{
+    gCount++;
+    static uint8_t count = 0;
+    if (gCount == 10)
+    {
+        TIMSK &= ~(1 << OCIE0); // turn off the section interrupt
+        OCR0 = (1 / (gRotations* RESOLUTION * TICKS)); // sets the new value for the timer0
+        TCNT0 = 0; // reset the timer
+        gRotations = 0;
+        TIMSK |= (1 << OCIE0);
+        gCount = 0;
+
+    }
+    // check the button states
+}
+
+
+/*
+ * Interrupt handler for when the sensor trips
+ */
+ISR(INT0_vect)
+{
+    gPlatterPos = 0;
+    gRotations++;
+}
+
+
+
+void set_duty_cycle(uint16_t width)
+{
+    OCR2 = ((width + 50) / PWM_TICK_WIDTH);
+}
+
+
+
+void init_ESC(void)
+{
+    TCCR2 = (1 << WGM21) | (1 << WGM20) | (1 << COM21);
+    TCCR2 |= (1 << CS22) | (1 << CS21) | (1 << CS20);
+    set_duty_cycle(500);
+    uint8_t i;
+    for (i = 0; i < 7; i++) {
+        set_color(cycle_color[i]);
+        _delay_ms(1000);
+    }
+    set_color(OFF);
+    set_duty_cycle(1200);
+}
+
+
+void set_color(uint8_t color) {
+    PORTD &= ~(WHITE);  // turn off all colors
+    PORTD |= color;     // turn on the one we want
 }
 
 
 
 int main(void)
 {
-    DDRD = (1 << PD3) | (1 << PD4) | (1 << PD5) | (1 << PD7);
+    DDRD |= (1 << RED_LED) | (1 << BLUE_LED) | (1 << GREEN_LED) | (1 << PWM);
+    init_ESC();
 
-    TCCR0 = (1 << WGM01); // sets timer0 to use CTC
-    OCR0 = 156;
-    TIMSK = (1 << OCIE0); // set the interrupt bit
-    sei(); // sets the I-bit
-    TCCR0 |= (1 << CS02) | (1 << CS00); // sets the prescaler and starts the timer
+    PORTD |= (1 << PD2); // should enable the pull-up
+    MCUCR = (1 << ISC01); // set as dropping edge
+
+    GICR = (1 << INT0); // turn on the external interrupt for the sensor
+
+    TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10); // enable 100 ms timer on timer1
+    OCR1A = 20000;
+
+    // turn on timer for the sections
+    TCCR0 = (1 << WGM01) | (1 << CS01);
+    TIMSK = (1 << OCIE1A);
+    TIMSK |= (1 << OCIE0); // enable the section interrupt
+    OCR0 = 180;
+
+    sei();
 
 
-
-
-    /* _delay_ms(1000); // sleep for 1 second */
-    /* TCCR2 = (1 << WGM21) | (1 << WGM20); // sets the mode to fast PWM */
-    /* TCCR2 |= (1 << COM21); // Sets the PWM signal to non-inverted */
-    /* TIMSK |= (1 << TOIE2); */
-    /* OCR2 = 64; // should be a .5 ms duty cycle to arm */
-    /* TCCR2 |= (1 << CS22) | (1 << CS20); // sets the prescaler to 128 */
-
-
-    /* _delay_ms(5000); // sleep for 5 seconds */
-
-    /* OCR2 = 128; */
-
-    while(1)
-    {
-    }
-
+    while(1);
     return 0;
 }
